e02e234cfc12a5c39668cc906124520d6173e146
Liqoo <liqoo.mumbahiz13@gmail.com>
2021-04-23 23:40:54 +0700

    [ADD] init commit

---
+++ src/main/java/app/TestPiplineAws.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPiplineAws {
+
+    public static void main(String[] args){
+        SpringApplication.run(TestPiplineAws.class, args);
+    }
+}
